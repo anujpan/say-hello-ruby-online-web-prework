ef0ac753b2bf8bd2eced48184e7e53a5b300e03b
Anuj Panchal <anujpan1@gmail.com>
2018-09-19 15:27:49 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 
-def say_hello(name, name="Fucker")
-  return ""
+def say_hello(name, name="Ruby Programmer")
+  return "Hello #{name}"
 end
 
 say_hello("Kent Beck")
