607e3cd96f4cd2f10ba3421605b592fd7b029d9d
Anuj Panchal <anujpan1@gmail.com>
2018-09-19 15:30:56 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,9 @@
 # Build your say_hello method here
 
 def say_hello(name)
-  puts "Hello #{name}"
-  print "Hello #{name}"
+  puts "Hello #{name}!"
+  print "Hello #{name}!"
   "Hello #{name}"
 end
 
-say_hello("Gabriela")
+say_hello("Kent Beck")
